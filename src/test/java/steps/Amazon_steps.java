37605b510397ba63a4f470bbb565383fcd825bcc
Tarik Gulbas <tarikgulbas@yahoo.com>
2019-02-05 11:32:21 -0500

    added Amazon inital test

--- src/test/java/steps/Amazon_steps.java
+++ src/test/java/steps/Amazon_steps.java
@@ -7,8 +7,6 @@ import utilities.Driver;
 
 public class Amazon_steps {
 
-
-
     @When("user goes to amazon website")
     public void user_goes_to_amazon_website() {
         Driver.getDriver().get("https://amazon.com");
@@ -16,15 +14,15 @@ public class Amazon_steps {
 
     @Then("verify it contains the word Amazon")
     public void verify_it_contains_the_word_Amazon() {
-        System.out.println(Driver.getDriver().getTitle());
-        Assert.assertTrue(Driver.getDriver().getTitle().contains("Amazon"));
-        Driver.getDriver().close();
+       Assert.assertTrue(Driver.getDriver().getTitle().contains("Amazon"));
+       Driver.closeDriver();
     }
 
     @Then("validate the url")
     public void validate_the_url() {
-        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),"https://www.amazon.com/");
-        Driver.getDriver().close();
+       Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("amazon"));
+       Driver.closeDriver();
     }
+
 
 }
--- src/test/resources/features/amazonTitle.feature
+++ src/test/resources/features/amazonTitle.feature
@@ -1,10 +1,11 @@
+
+@Amazon
 Feature: Amazon Title
-  @Amazon
+
   Scenario: Amazon Title verification
     When user goes to amazon website
     Then verify it contains the word Amazon
 
-  @Amazon
     Scenario: Amazon url verification
       When user goes to amazon website
       Then validate the url
